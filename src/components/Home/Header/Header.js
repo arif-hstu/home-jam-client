bff3e9dfcffd2af1abf47c13d6197eb224252d3b
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-12 16:43:15 +0600

    scrollable dashboard

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import { Scroll } from "framer";
 
 import Navbar from '../../Shared/Navbar/Navbar';
 import './Header.css';
 import headerBg from '../../../resources/images/headerBg.png';
+import lader from '../../../resources/icons/lader.svg';
+import laderInactive from '../../../resources/icons/laderInactive.svg';
 
 function Header() {
 	return (
 		<div className='Header'>
 			<Navbar />
 			<div className="overlay">
-			This is overlay
+			</div>
+			<div className="mainContent">
+				<div className="titleHolder">
+					<h2>Cari Cari</h2>
+					<p>Live from their sofa to yours. Get closer to your favorite <br />artists, and never miss out.</p>
+				</div>
+				<Scroll
+					className='scroll'
+					direction="horizontal"
+					width={'100%'}
+					height={'208px'}
+				>
+					<div className="dashboard">
+						<div className="active">
+							<img src={lader} alt="" />
+						</div>
+						<div className="default">
+							<img src={laderInactive} alt="" />
+						</div>
+						<div className="default">
+								<img src={laderInactive} alt="" />
+						</div>
+						<div className="default">
+								<img src={laderInactive} alt="" />
+						</div>
+					</div>
+				</Scroll>
 			</div>
 		</div>
 	)
